2a5b087fdf337c9f798540043a34b67706d46795
LucasBrunner <lucasbrunner@gmail.com>
2022-11-04 15:04:46 -0400

    create project

---
+++ src/main/java/cen4333/group2/App.java
@@ -0,0 +1,13 @@
+package cen4333.group2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
